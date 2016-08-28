616174b972b4d9c94aa4a1ae28c9bcba4e120732
dzysghr <dzysghr@live.com>
2016-08-28 21:21:25 +0800

    url增加版本信息

--- app.js
+++ app.js
@@ -7,54 +7,20 @@ var account = require("./router/account_router")
 var project = require("./router/project_router")
 var task = require('./router/task_router')
 var message = require('./router/message_router')
-var formidable = require('formidable')
-var util = require('util')
-var fs = require('fs');
 
 app.use(cookieParser());
-app.use('/images', express.static('image'));
-app.use('/user', user);
-app.use('/team', team);
-app.use('/project', project);
-app.use('/account', account);
-app.use('/task', task);
- app.use('/message',message);
+app.use('/v1/images', express.static('image'));
+app.use('/v1/user', user);
+app.use('/v1/team', team);
+app.use('/v1/project', project);
+app.use('/v1/account', account);
+app.use('/v1/task', task);
+app.use('/v1/message',message);
 
 app.all('/', function (req, res) {
     console.log("应用实例，主机名 ：%s  IP:%s", req.hostname, req.ip);
     res.send('hello world');
 
-})
-
-
-
-//todo delete
-app.post('/upload', function (req, res) {
-
-    var contentype = req.headers['content-type'] + '';
-    if (contentype == undefined || contentype.indexOf('multipart/form-data') != 0) {
-        res.send('error conten  type');
-        return;
-    }
-
-    var form = new formidable.IncomingForm();
-    form.uploadDir = __dirname + "/image";
-    form.encoding = 'utf-8';		//设置编辑
-    form.keepExtensions = true;	 //保留后缀
-    form.maxFieldsSize = 2 * 1024 * 1024;   //文件大小
-    form.parse(req, function (err, fields, files) {
-        if (err) {
-            res.send(bodymaker.makeErrorJson(7, err));
-            return;
-        }
-        if (files.length == 0 || files['img'] == undefined) {
-            res.send(bodymaker.makeErrorJson(7, 'file not found,"img" key is respected'));
-            return;
-        }
-        var newPath = form.uploadDir + '/abc_avatar';
-        fs.renameSync(files['img'].path, newPath);  //重命名
-        res.send(files['img']);
-    })
 })
 
 
